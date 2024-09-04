9b0972f147468346251fc454245723a83f50d27a
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2024-09-04 08:37:33 -0300

    feat: getRandomBlock functions

--- src/index.js
+++ src/index.js
@@ -17,3 +17,38 @@ const player2 = {
 async function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
+
+async function getRandomBlock() {
+    let random = Math.random();
+    let result = "";
+
+    switch(true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break;
+        default:
+            result = "CONFRONTO";
+    }
+    
+    return result;
+}
+
+async function playRaceEngine(character1, character2) {
+    for(let round = 1; round <= 5; round++) {
+        console.log(`🏁 Rodada ${round}:\n`);
+        // sortear bloco
+        let bloco = await getRandomBlock();
+        console.log(`Bloco sorteado: ${bloco}\n`);
+    }
+}
+
+(async function main() {
+    console.log(
+        `🏁🚨 Corrida entre ${player1.name} and ${player2.name} começando ...\n`
+    );
+
+    await playRaceEngine(player1, player2);
+})();
